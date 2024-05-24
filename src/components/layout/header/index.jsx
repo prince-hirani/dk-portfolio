45c4a9663674c0756e6bfbf3544597d5904954ad
DkTechanzo <dk.techanzo@gmail.com>
2024-05-24 20:10:34 +0530

    create header

--- src/components/layout/header/index.jsx
+++ src/components/layout/header/index.jsx
@@ -1,5 +1,54 @@
-function Header() {
-  return <div>Header</div>;
+import { Link, Stack, Typography } from "@mui/material";
+import { ImgLogo, LinksBody, Root, StlyedButton, StyledLink } from "./styles";
+import Logo from "../../../assets/icons/logo.svg";
+import { ReactComponent as GreenDot } from "../../../assets/icons/green-dot.svg";
+import clsx from "clsx";
+
+const links = [
+  { label: "Home", path: "/" },
+  { label: "About Me", path: "about-me" },
+  { label: "My Work", path: "my-work" },
+];
+
+function Header({ active }) {
+  return (
+    <Root direction="row" justifyContent="space-between">
+      <Link to="/">
+        <ImgLogo src={Logo} />
+      </Link>
+
+      <LinksBody>
+        <Stack direction="row" gap={1}>
+          {links.map((link, index) => {
+            return (
+              <StyledLink
+                key={index}
+                to={link.path}
+                className={clsx({
+                  active: link.label.toLowerCase() === active,
+                })}
+              >
+                {link.label}
+              </StyledLink>
+            );
+          })}
+        </Stack>
+      </LinksBody>
+      <Stack>
+        <StlyedButton variant="md">
+          <GreenDot />
+          <Typography
+            pl={0.75}
+            fontSize="0.875rem"
+            fontWeight="500"
+            lineHeight="18.2px"
+          >
+            Contact me
+          </Typography>
+        </StlyedButton>
+      </Stack>
+    </Root>
+  );
 }
 
 export default Header;
---
+++ src/components/layout/header/styles.js
@@ -0,0 +1,33 @@
+import { Button, Stack, styled } from "@mui/material";
+import { Link } from "react-router-dom";
+
+export const Root = styled(Stack)(({ theme }) => ({
+  padding: theme.spacing(2.5, 39, 0),
+}));
+
+export const ImgLogo = styled("img")(({ theme }) => ({
+  cursor: "pointer",
+}));
+
+export const StyledLink = styled(Link)(({ theme }) => ({
+  color: theme.palette.grey[900],
+  textDecoration: "none",
+  cursor: "pointer",
+  padding: theme.spacing(1, 2.25),
+
+  "&.active": {
+    color: theme.palette.grey[500],
+  },
+}));
+
+export const LinksBody = styled(Stack)(({ theme }) => ({
+  border: `1px solid ${theme.palette.grey[700]}`,
+  borderRadius: "50px",
+  padding: theme.spacing(0.5),
+}));
+export const StlyedButton = styled(Button)(({ theme }) => ({
+  cursor: "pointer",
+  backgroundColor: theme.palette.grey[700],
+  borderRadius: "8px",
+  padding: theme.spacing(2, 3.75),
+}));
