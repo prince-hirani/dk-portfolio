eeb67c1ac36c9372a24288b32d6011bed5a7ba43
DkTechanzo <dk.techanzo@gmail.com>
2024-05-27 11:55:04 +0530

    change header

--- src/components/layout/header/styles.js
+++ src/components/layout/header/styles.js
@@ -2,7 +2,7 @@ import { Button, Stack, styled } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export const Root = styled(Stack)(({ theme }) => ({
-  padding: theme.spacing(2.5, 1, 0),
+  padding: theme.spacing(2.5, 2),
 }));
 
 export const ImgLogo = styled("img")(({ theme }) => ({
