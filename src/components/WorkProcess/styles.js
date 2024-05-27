29d85bf34b92c49b4cf215b3b96aeca0f894eeb6
DkTechanzo <dk.techanzo@gmail.com>
2024-05-27 11:56:01 +0530

    change workprocess

--- src/components/WorkProcess/styles.js
+++ src/components/WorkProcess/styles.js
@@ -1,6 +1,6 @@
 import { Stack, styled } from "@mui/material";
 export const Root = styled(Stack)(({ theme }) => ({
-  padding: theme.spacing(0, 1),
+  padding: theme.spacing(0, 2),
 }));
 export const Card = styled(Stack)(({ theme }) => ({
   border: `1px solid ${theme.palette.grey[400]}`,
